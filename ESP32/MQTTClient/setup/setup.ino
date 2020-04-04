3ec457f24dbd0acdce851bd41d3f387de23384d0
Enrique Perez <enrike_084@hotmail.com>
2020-04-04 17:56:46 -0600

    Se corrigieron y se agregaron nuevas evidencias y se agrego el codigo del esp32

---
+++ ESP32/MQTTClient/setup/setup.ino
@@ -0,0 +1,113 @@
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "IZZI-AEB0";
+const char* password = "8871B1BEAEB0";
+
+// Add your MQTT Broker IP address, example:
+//const char* mqtt_server = "192.168.0.144";
+const char* mqtt_server = "192.168.0.250";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// LED Pin
+const int ledPin = 2;
+
+void setup() {
+  Serial.begin(115200);
+  
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Conectando a :");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Mensaje recibido en topico: ");
+  Serial.print(topic);
+  Serial.print(". Mensaje: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Estableciendo la conexion MQTT...");
+    // Attempt to connect
+    if (client.connect("ESP32")) {
+      Serial.println("Conectado");
+      // Subscribe
+      client.subscribe("test/message");
+    } else {
+      Serial.print("Error de conexion, rc=");
+      Serial.print(client.state());
+      Serial.println("Intentando en 5 segundos");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    char* mensaje = "mensaje desde la esp32";
+    
+    client.publish("esp32/test", mensaje);
+
+  }
+}
