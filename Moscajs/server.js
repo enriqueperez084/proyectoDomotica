b69f1f858c66fa2f79c8cb42b75e927690e912ed
Enrique Perez <enrike_084@hotmail.com>
2020-04-16 14:34:28 -0500

    Servidor Finalizado

--- Moscajs/server.js
+++ Moscajs/server.js
@@ -1,27 +1,24 @@
 'use strict'
 
+const url = 'mongodb+srv://db_user_1:wiPHUJh9Fj0drytL@cluster0-dtp5z.mongodb.net/chat_nodejs_db'
+const db = require('./db')
 const mosca = require('mosca')
+const router = require('./routes')
 const chalk = require('chalk')
 
-/*const mongo = require('mongo')
+
+db(url)
 
 const backend = {
-  type: 'mongo',
-  url: 'mongodb://admin:admin@localhost:27017/platziverse-mqtt?authSource=admin',
-  mongo,
-  return_buffers: true
-}*/
-const pubsubsettings = {
     type: 'mongodb',
-    //url: "mongodb+srv://db_user_1:wiPHUJh9Fj0drytL@cluster0-dtp5z.mongodb.net/test?retryWrites=true&w=majority",
-    //pubsubCollection: 'ascoltatori',
+    url : url,
     mongo: {},
     return_buffers: true
 }
 
 const moscaSettings = {
     port: 1883,
-    backend: pubsubsettings
+    backend
 }
 const server = new mosca.Server(moscaSettings)
 const clients = new Map()
@@ -37,16 +34,7 @@ server.on('clientDisconnected', client => {
 
 server.on('published', (packet, client) => {
     console.log(`${chalk.magenta.underline('Recibido de:')} ${packet.topic}`)
-    switch (packet.topic) {
-        case 'esp32/test':
-            //server.publish(message)
-            break
-        case 'test/message':
-            //console.log('Se recibio un mensaje de test/message')
-            console.log(`${chalk.magenta.underline('Payload:')} ${packet.payload}`)
-            break
-    }
-
+    router(packet, client)
 })
 
 server.on('ready', () => {
